46d13be59f105a9370e22823a8df970cf39dac9f
Keen Lok <keenloklai@gmail.com>
2019-03-29 17:45:22 +0800

    Added some stylesheets from templates and an additional query

--- routes/search.js
+++ routes/search.js
@@ -6,14 +6,14 @@ const pool = new Pool({
     //ssl: true
 });
 const utils = require('./utils/util')
-const searchQuery = require('./sql/sqlQueries').findRestaurant
+const queries = require('./sql/sqlQueries')
 
 router.get('/', (req, res, next) => {
     let ctx = 0, avg = 0, table
     let queryStr = req.query.restaurant;
     let rname = '%' + queryStr.toLowerCase() + '%'
+    let searchQuery = queries.findRestaurant
     let time = utils.getTime();
-    //console.log(rname)
 
     pool.query(searchQuery, [time, rname], (err, data) => {
         if (err || !data.rows || data.rows.length === 0) {
@@ -26,5 +26,25 @@ router.get('/', (req, res, next) => {
         res.render('search', {page: 'Search Results', query: queryStr, table: table, ctx: ctx})
     })
 })
+
+router.get('/postalcode', (req, res, next) => {
+    let ctx = 0, avg = 0, table
+    let searchQuery = queries.findWithPostCode;
+    let queryStr = req.query.postalcode;
+    let time = utils.getTime();
+    //console.log(rname)
+
+    pool.query(searchQuery, [time, queryStr], (err, data) => {
+        if (err || !data.rows || data.rows.length === 0) {
+            ctx = 0
+            table = []
+        } else {
+            ctx = data.rows.length
+            table = data.rows
+        }
+        res.render('search', {page: 'Search Results', query: queryStr, table: table, ctx: ctx})
+    })
+})
+
 
 module.exports = router
--- routes/sql/sqlQueries.js
+++ routes/sql/sqlQueries.js
@@ -2,11 +2,12 @@
 const available_restaurants = 'SELECT rname, location FROM ((advertises  natural join restaurants) natural join branches) R WHERE EXISTS ( SELECT 1 FROM freetables F WHERE F.rid = R.rid AND F.bid = R.bid AND F.availableSince < $1);';
 const branch_w_status = 'WITH restaurantStatus AS ( SELECT rid, bid, COUNT(tid) AS num FROM freetables GROUP BY rid, bid ) SELECT rname, location, CASE WHEN (opentime > $1 OR closeTime < $1) THEN \'CLOSED\' WHEN num > 0 THEN \'AVAILABLE\' WHEN num = 0 THEN \'FULL\' END AS status FROM (advertises NATURAL JOIN restaurants NATURAL JOIN branches) R LEFT JOIN restaurantStatus S ON R.rid = S.rid AND R.bid = S.bid;';
 const get_restaurant = 'WITH restaurantStatus AS ( SELECT rid, bid, COUNT(tid) AS num FROM freetables GROUP BY rid, bid ) SELECT DISTINCT rname, location, openinghours, opentime, closetime, CASE WHEN (opentime > $1 OR closeTime < $1) THEN \'CLOSED\' WHEN num > 0 THEN \'AVAILABLE\' WHEN num = 0 THEN \'FULL\' END AS status FROM (restaurants R LEFT JOIN branches B ON R.rid = B.rid) LEFT JOIN restaurantStatus S ON R.rid = S.rid WHERE R.rname = $2;';
-const find_restaurant = 'WITH restaurantStatus AS ( SELECT rid, bid, COUNT(tid) AS num FROM freetables GROUP BY rid, bid ) SELECT distinct rname, openinghours, CASE WHEN (opentime > $1 OR closeTime < $1) THEN \'CLOSED\' WHEN num > 0 THEN \'AVAILABLE\' WHEN num = 0 THEN \'FULL\' END AS status FROM (restaurants NATURAL JOIN branches) R LEFT JOIN restaurantStatus S ON R.rid = S.rid WHERE LOWER(rname) LIKE $2;'
-
+const find_restaurant = 'WITH restaurantStatus AS ( SELECT rid, bid, COUNT(tid) AS num FROM freetables GROUP BY rid, bid ) SELECT distinct rname, openinghours, location, CASE WHEN (opentime > $1 OR closeTime < $1) THEN \'CLOSED\' WHEN num > 0 THEN \'AVAILABLE\' WHEN num = 0 THEN \'FULL\' END AS status FROM (restaurants NATURAL JOIN branches) R LEFT JOIN restaurantStatus S ON R.rid = S.rid WHERE LOWER(rname) LIKE $2;'
+const find_postal_code = 'WITH restaurantStatus AS ( SELECT rid, bid, COUNT(tid) AS num FROM freetables GROUP BY rid, bid ) SELECT DISTINCT rname, openinghours, location, CASE WHEN (opentime > $1 OR closeTime < $1) THEN \'CLOSED\' WHEN num > 0 THEN \'AVAILABLE\' WHEN num = 0 THEN \'FULL\' END AS status FROM (restaurants R LEFT JOIN branches B ON R.rid = B.rid) LEFT JOIN restaurantStatus S ON R.rid = S.rid WHERE ABS(B.postalCode - $2) <= 9999;'
 
 module.exports = {
     findAllAvailableRestaurants: available_restaurants,
     allBranchWithStatus: branch_w_status,
-    findRestaurant: find_restaurant
+    findRestaurant: find_restaurant,
+    findWithPostCode: find_postal_code,
 };

f0ce85f4100ed49de712de3a9246fab6175f962b
Aaron Seah <aaronsgseah@gmail.com>
2019-04-08 14:32:16 +0800

    added time table and inserted time values into the table Time

--- sqlScripts/init.sql
+++ sqlScripts/init.sql
@@ -18,6 +18,12 @@ drop table if exists menuItems  cascade;
 drop table if exists Sells  cascade;
 drop table if exists CuisineTypes cascade;
 drop table if exists Locations cascade;
+drop table if exists Time cascade;
+
+
+create table Time (
+timeSlot time primary key
+);
 
 create table Diners (
 userName varchar(20) primary key,
@@ -154,6 +160,31 @@ delete from CuisineTypes cascade;
 delete from menuitems cascade;
 delete from menu cascade;
 delete from Locations cascade;
+delete from Time cascade;
+
+
+insert into Time (timeSlot) values 
+('05:00:00'), ('05:15:00'), ('05:30:00'), ('05:45:00'),
+('06:00:00'), ('06:15:00'), ('06:30:00'), ('06:45:00'),
+('07:00:00'), ('07:15:00'), ('07:30:00'), ('07:45:00'),
+('08:00:00'), ('08:15:00'), ('08:30:00'), ('08:45:00'),
+('09:00:00'), ('09:15:00'), ('09:30:00'), ('09:45:00'),
+('10:00:00'), ('10:15:00'), ('10:30:00'), ('10:45:00'),
+('11:00:00'), ('11:15:00'), ('11:30:00'), ('11:45:00'),
+('12:00:00'), ('12:15:00'), ('12:30:00'), ('12:45:00'),
+('13:00:00'), ('13:15:00'), ('13:30:00'), ('13:45:00'),
+('14:00:00'), ('14:15:00'), ('14:30:00'), ('14:45:00'),
+('15:00:00'), ('15:15:00'), ('15:30:00'), ('15:45:00'),
+('16:00:00'), ('16:15:00'), ('16:30:00'), ('16:45:00'),
+('17:00:00'), ('17:15:00'), ('17:30:00'), ('17:45:00'),
+('18:00:00'), ('18:15:00'), ('18:30:00'), ('18:45:00'),
+('19:00:00'), ('19:15:00'), ('19:30:00'), ('19:45:00'),
+('20:00:00'), ('20:15:00'), ('20:30:00'), ('20:45:00'),
+('21:00:00'), ('21:15:00'), ('21:30:00'), ('21:45:00'),
+('22:00:00'), ('22:15:00'), ('22:30:00'), ('22:45:00'),
+('23:00:00'), ('23:15:00'), ('23:30:00'), ('23:45:00');
+
+
 
 insert into restaurants (rname) values
 ('MacDonalds'),
@@ -281,6 +312,7 @@ insert into Sells (menuname, rname, bid) values
 ('BurgerKing Breakfast Menu', 'BurgerKing', 5),
 ('Crystal Jade Main Menu', 'Crystal Jade', 1)
 ;
+
 
 
 
